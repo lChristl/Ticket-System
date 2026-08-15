27e08be19c9ae58a6d120735d62981c0ea1d5b3d
Christian F. De Leon <chris20170322@gmail.com>
2026-08-15 22:08:17 +0800

    define buy as the function to call out the code in main.py

--- Add.py
+++ Add.py
@@ -6,53 +6,54 @@ movie = (
     ("Blue-Lock Live Action", 250, "August 07, 2026",
     ("11:00AM", "4:00PM", "9:00PM"))
 )
-print("====Buy Ticket====")
-print("Movie Showing!!");
-print("1. Spider-Man:Brand New Day : P285 : July 31, 2026");
-print("2. The Odyssey  : P255 : July 15, 2026");
-print("3. Blue-Lock Live Action : P250 : August 07, 2026");
+def buy():
+    print("====Buy Ticket====")
+    print("Movie Showing!!")
+    print("1. Spider-Man:Brand New Day : P285 : July 31, 2026")
+    print("2. The Odyssey  : P255 : July 15, 2026")
+    print("3. Blue-Lock Live Action : P250 : August 07, 2026")
 
-choice = int(input("Movie (Enter a number): "))
-if choice == 1:
-    movie, price, date, time = movie[0]
+    choice = int(input("Movie (Enter a number): "))
+    if choice == 1:
+        movie, price, date, time = movie[0]
 
-    print("====CINEMA 1 - Time Available====")
-    print("1. 12:30PM")
-    print("2. 3:30PM")
-    print("3. 6:30PM")
+        print("====CINEMA 1 - Time Available====")
+        print("1. 12:30PM")
+        print("2. 3:30PM")
+        print("3. 6:30PM")
 
-elif choice == 2:
-    movie, price, date, time = movie[1]
+    elif choice == 2:
+        movie, price, date, time = movie[1]
 
-    print("====CINEMA 2 - Time Available====")
-    print("1. 1:10PM")
-    print("2. 5:00PM")
-    print("3. 9:00PM")
-    
-elif choice == 3:
-    movie, price, date, time = movie[2]
+        print("====CINEMA 2 - Time Available====")
+        print("1. 1:10PM")
+        print("2. 5:00PM")
+        print("3. 9:00PM")
+        
+    elif choice == 3:
+        movie, price, date, time = movie[2]
 
-    print("====CINEMA 3 - Time Available====")
-    print("1. 11:00AM")
-    print("2. 4:00PM")
-    print("3. 9:00PM")
-else:
-    print("Invalid!!")
+        print("====CINEMA 3 - Time Available====")
+        print("1. 11:00AM")
+        print("2. 4:00PM")
+        print("3. 9:00PM")
+    else:
+        print("Invalid!!")
 
-showtime_choice = int(input("Time (Enter a Number): "))
-if showtime_choice == 1:
-    showtime = time[0]
+    showtime_choice = int(input("Time (Enter a Number): "))
+    if showtime_choice == 1:
+        showtime = time[0]
 
-elif showtime_choice == 2:
-    showtime = time[1]
+    elif showtime_choice == 2:
+        showtime = time[1]
 
-elif showtime_choice == 3:
-    showtime = time[2]
-else:
-    print("Invalid!!")
+    elif showtime_choice == 3:
+        showtime = time[2]
+    else:
+        print("Invalid!!")
 
-quantity = int(input("Number of Ticket: "))
-total = price * quantity
+    quantity = int(input("Number of Ticket: "))
+    total = price * quantity
 
-print(f"Total: P{total}")
-print(f"Time: {showtime}")
+    print(f"Total: P{total}")
+    print(f"Time: {showtime}")
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-#import add
+import Add
 #import view
 #import update
 
-print("Ticket System");
-print("What would you like to do?");
-print("1. View Movies"); #view
-print("2. Order ticket"); #add
-print("3. Update current tickets"); #update
+print("Ticket System")
+print("What would you like to do?")
+print("1. View Movies") #view
+print("2. Order ticket") #add
+print("3. Update current tickets") #update
 
 
-print("Input choice here");
-choice = input();
+print("Input choice here")
+choice = input()
 
-if choice == "1":
-    print();
-elif choice == "2":
-    print();
-elif choice == "3":
-    print();
+if choice == "1": #view
+    print()
+elif choice == "2": #add
+    Add.buy()
+elif choice == "3": #update
+    print()
 else:
-    print("Input only numbers [1]-[3]");
+    print("Input only numbers [1]-[3]")
 
 # view()
 # add()
