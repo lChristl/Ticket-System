47e39750254b518baeefff437e3d5bc3220b7b49
Christian F. De Leon <chris20170322@gmail.com>
2026-08-16 22:17:25 +0800

    currently adding functionality for bookings

--- update.py
+++ update.py
@@ -1,18 +1,31 @@
 import data
 
 
-BOOKINGS = {}
+BOOKINGS = {
+    "1":{
+        "title":"Testing movie 1",
+        "price":"",
+    }
+            
+
+            }
 
 def modify_booking():
     print()
     if not BOOKINGS:
         print("No active bookings found.")
         return
-
-    ticket_id = input("Enter your Ticket ID (e.g., 1)")
     
+    ticket_id = input("Enter your Ticket ID (e.g., 1)")
+    if ticket_id not in BOOKINGS:
+        print("Booking not found!")
+        return
 
-
+    booking = BOOKINGS[ticket_id]
+    print(f"Current Booking: {booking["title"]}")
+    print("")
+    print("")
+    print("")
 
 
 
