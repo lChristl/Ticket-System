8fc801be1dfef271ef166956fa0c5778cfea5c43
paolaine0721-beep <paolaine0721@gmail.com>
2026-08-16 15:14:30 +0000

    fix system of viewing ticket

--- view.py
+++ view.py
@@ -5,12 +5,12 @@ def view_tickets():
 
     if not tickets:
         print("No tickets purchased yet.\n")
-
-    for index, ticket in enumerate(tickets, start=1):
-    print(f"Ticket: {index}")
-    print(f" Movie: {ticket['movie']}")
-    print(f" Date: {ticket['date']}")
-    print(f" Time: {ticket['showtime']}")
-    print(f" Quantity: {ticket['quantity']}")
-    print(f" Total: P{ticket['total']}")
-    print ("-" * 20)
+    else:
+        for index, ticket in enumerate(tickets, start=1):
+            print(f"Ticket: {index}")
+            print(f" Movie: {ticket['movie']}")
+            print(f" Date: {ticket['date']}")
+            print(f" Time: {ticket['showtime']}")
+            print(f" Quantity: {ticket['quantity']}")
+            print(f" Total: P{ticket['total']}")
+            print("-" * 20)
