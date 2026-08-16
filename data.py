af92db40930cc0f3bfba858c1782c967a50b1cee
Christian F. De Leon <chris20170322@gmail.com>
2026-08-16 20:14:27 +0800

    edit data

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
-movies = (
-    ("Spider-Man:Brand New Day", 285, "July 31, 2026",
-    ("12:30PM", "3:30PM", "6:30PM")),
-    ("The Odyssey", 255, "July 15, 2026",
-    ("1:10PM", "7:00PM", "9:00PM")),
-    ("Blue-Lock Live Action", 250, "August 07, 2026",
-    ("11:00AM", "4:00PM", "9:00PM"))
-)
+
+movies = [
+    ("Spider-Man: Brand New Day", 285, "July 31, 2026", ["12:30PM", "3:30PM", "6:30PM"]),
+    ("The Odyssey", 255, "July 15, 2026", ["1:10PM", "5:00PM", "9:00PM"]),
+    ("Blue-Lock Live Action", 250, "August 07, 2026", ["11:00AM", "4:00PM", "9:00PM"])
+]
+
+tickets = []
