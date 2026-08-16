4c15530950dd7b79c3801b619aff093fdf4bc409
Christian F. De Leon <chris20170322@gmail.com>
2026-08-16 22:38:04 +0800

    Add option 1 for option to update booking

--- update.py
+++ update.py
@@ -4,7 +4,11 @@ import data
 BOOKINGS = {
     "1":{
         "title":"Testing movie 1",
-        "price":"",
+        "price":100,
+        "time":"12:00PM",
+        "quantity":"2",
+        "total": 200,
+        "times_available": ("1:10PM","5:10PM","7:10PM")
     }
             
 
@@ -23,11 +27,18 @@ def modify_booking():
 
     booking = BOOKINGS[ticket_id]
     print(f"Current Booking: {booking["title"]}")
-    print("")
-    print("")
-    print("")
+    print("1. Change Showtime")
+    print("2. Change Ticket Quantity")
+    print("3. Cancel Booking")
 
+    option = input("Choose an option: ")
 
+    if option == "1":
+        print("Available Showtimes: ")
+        for index, time in enumerate(booking["times_available"], 1):
+            print(f"{index}. {time}")
+
+        t_choice = int(input("Select new showtime number: "))
 
 modify_booking()
     
