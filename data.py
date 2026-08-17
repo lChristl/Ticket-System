3d28a6caf05da95b6281f1936b196c925dbf75a0
Christian F. De Leon <chris20170322@gmail.com>
2026-08-18 01:00:24 +0800

    removed old placeholder data and changed from list into dictionary

--- data.py
+++ data.py
@@ -1,11 +1,4 @@
 
-# movies = [
-#     ("Spider-Man: Brand New Day", 285, "July 31, 2026", ["12:30PM", "3:30PM", "6:30PM"]),
-#     ("The Odyssey", 255, "July 15, 2026", ["1:10PM", "5:00PM", "9:00PM"]),
-#     ("Blue-Lock Live Action", 250, "August 07, 2026", ["11:00AM", "4:00PM", "9:00PM"])
-# ]
-
-
 movies = {
     "1":{
     "title":"Spider-Man: Brand New Day",
@@ -28,26 +21,4 @@ movies = {
     
 
 }
-
-tickets = []
-
-BOOKINGS = {
-    # "1":{
-    #     "title":"Testing movie 1",
-    #     "price":100,
-    #     "time":"12:00PM",
-    #     "quantity":"2",
-    #     "total": 200,
-    #     "times_available": ("1:10PM","5:10PM","7:10PM")
-    # },
-
-    # "2":{
-    #         "title":"Testing movie 2",
-    #         "price":100,
-    #         "time":"12:00PM",
-    #         "quantity":"2",
-    #         "total": 200,
-    #         "times_available": ("1:10PM","5:10PM","7:10PM")
-    #     }
-
-            }
+BOOKINGS = {}
